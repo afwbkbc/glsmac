3ad7cad284962a7a995a51e50382d4a9a715a52a
afwbkbc <afwbkbc@inbox.lv>
2023-02-11 14:21:16 +0200

    changed -O0 to -Og for debug builds

---
+++ src/ui/object/Mesh.cpp
@@ -0,0 +1,110 @@
+#include "Surface.h"
+
+using namespace types;
+
+namespace ui {
+namespace object {
+
+Surface::Surface( const string& class_name )
+	: UIObject( class_name )
+{
+	//
+}
+
+void Surface::SetTexture( const types::Texture* texture ) {
+	if ( texture != m_texture ) {
+		//Log( "Setting texture " + texture->m_name );
+		m_texture = texture;
+		if ( m_background ) {
+			m_background->SetTexture( m_texture );
+		}
+		Realign();
+	}
+}
+
+void Surface::ClearTexture() {
+	if ( m_texture ) {
+		//Log( "Clearing texture" );
+		m_texture = nullptr;
+		if ( m_background ) {
+			m_background->SetTexture( nullptr );
+		}
+	}
+}
+
+void Surface::SetStretchTexture( const bool stretch_texture ) {
+	ASSERT( m_texture, "stretch texture but texture not set" );
+	if ( stretch_texture != m_stretch_texture ) {
+		m_stretch_texture = stretch_texture;
+		Realign();
+	}
+}
+
+void Surface::Create() {
+	UIObject::Create();
+
+	NEW( m_background_mesh, types::mesh::Rectangle );
+	NEW( m_background, scene::actor::Mesh, "UI::Surface", m_background_mesh );
+	AddActor( m_background );
+}
+
+void Surface::Destroy() {
+
+	RemoveActor( m_background );
+	DELETE( m_background );
+
+	UIObject::Destroy();
+}
+
+void Surface::Align() {
+	UIObject::Align();
+
+	const Vec2<coord_t> v1 = { ClampX( m_object_area.left ), ClampY( m_object_area.top ) };
+	const Vec2<coord_t> v2 = { ClampX( m_object_area.right ), ClampY( m_object_area.bottom ) };
+	
+	if ( m_texture && !m_stretch_texture ) {
+		m_background_mesh->SetCoords( v1, v2, { m_texture->m_width, m_texture->m_height }, -m_z_index );
+	}
+	else {
+		m_background_mesh->SetCoords( v1, v2, -m_z_index );
+	}
+	//m_background->Update();
+}
+
+void Surface::Draw() {
+	UIObject::Draw();
+
+	if ( m_texture ) {
+		m_background->SetTexture( m_texture );
+	}
+}
+
+void Surface::ApplyStyle() {
+	UIObject::ApplyStyle();
+	
+	if ( Has( Style::A_TEXTURE ) ) {
+		const auto* texture = (Texture*)GetObject( Style::A_TEXTURE );
+		if ( texture ) {
+			SetTexture( texture );
+			if ( Has( Style::A_SIZE_FROM_TEXTURE ) ) {
+				SetWidth( texture->m_width );
+				SetHeight( texture->m_height );
+			}
+			if ( Has( Style::A_STRETCH_TEXTURE ) ) {
+				SetStretchTexture( true );
+			}
+			if ( Has( Style::A_KEEP_TEXTURE_ASPECT_RATIO ) ) {
+				ForceAspectRatio( (float) texture->m_height / texture->m_width );
+			}
+		}
+		else {
+			ClearTexture();
+		}
+	}
+	else {
+		ClearTexture();
+	}
+}
+
+}
+}
---
+++ src/ui/object/Mesh.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "types/mesh/Rectangle.h"
+
+#include "scene/actor/Mesh.h"
+
+#include "UIObject.h"
+
+#include "types/Color.h"
+#include "types/Texture.h"
+
+namespace ui {
+namespace object {
+
+CLASS( Surface, UIObject )
+
+	Surface( const string& class_name = "" );
+	
+	void SetTexture( const types::Texture* texture);
+	void ClearTexture();
+	void SetStretchTexture( const bool stretch_texture );
+	
+	virtual void Create();
+	virtual void Destroy();
+	virtual void Align();
+	virtual void Draw();
+	
+	
+protected:
+	
+	virtual void ApplyStyle();
+	
+	const types::Texture* m_texture = nullptr;
+	bool m_stretch_texture = false;
+
+	scene::actor::Mesh *m_background = nullptr;
+	types::mesh::Rectangle *m_background_mesh = nullptr;
+};
+
+} /* namespace object */
+} /* namespace ui */
