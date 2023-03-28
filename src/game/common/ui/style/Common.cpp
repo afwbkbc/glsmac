7d1b935dcea9cda3c1579d5e7854bf9acb1aaec3
afwbkbc <afwbkbc@inbox.lv>
2023-03-28 19:49:35 +0300

    fixed fps counter

--- src/game/common/ui/style/Common.cpp
+++ src/game/common/ui/style/Common.cpp
@@ -11,6 +11,7 @@ void Common::AddStyles() {
 		s->Set( Style::A_TOP, 0 );
 		s->SetFont( Style::A_FONT, "arialnb.ttf", 18 );
 		s->SetColor( Style::A_TEXT_COLOR, Color( 0.5f, 1.0f, 0.3f, 0.7f ) );
+		s->Set( Style::A_Z_INDEX, 0.9f );
 	});
 	
 }
