3a0c45693eed117c222e1a97bf8bf00edea2726b
afwbkbc <afwbkbc@inbox.lv>
2024-02-19 00:17:05 +0200

    fixed release build

--- src/game/rules/Rules.h
+++ src/game/rules/Rules.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <unordered_map>
+#include <vector>
 
 #include "types/Serializable.h"
 
--- src/types/Texture.h
+++ src/types/Texture.h
@@ -2,6 +2,7 @@
 
 #include <string>
 #include <vector>
+#include <unordered_map>
 
 #include "types/Serializable.h"
 
