f04d162ed3b1573c19833bac71572fdf06f2b983
afwbkbc <afwbkbc@inbox.lv>
2026-03-29 17:32:42 +0300

    base screen dummy top buttons

--- GLSMAC_data/default/ui/parts/game/popup/base_screen/top_buttons.gls.js
+++ GLSMAC_data/default/ui/parts/game/popup/base_screen/top_buttons.gls.js
@@ -11,6 +11,100 @@ return {
 			border: 'rgb(232,136,8),2',
 		});
 
+		p.ui.class('base-screen-top-button').set({
+			width: 112,
+			top: 3,
+			bottom: 3,
+			font: 'arialnb.ttf:16',
+			color: 'rgb(228,104,24)',
+			sound: 'ok.wav',
+			_hover: {
+				color: 'black',
+			},
+			_active: {
+				color: 'black',
+			},
+		});
+
+		p.ui.class('base-screen-top-button-1').extend('base-screen-top-button').set({
+			border: 'rgb(136,69,28),2',
+			background: 'interface.pcx:crop(68,65,134,82)',
+			_hover: {
+				background: 'interface.pcx:crop(68,86,134,103)',
+			},
+			_active: {
+				background: 'interface.pcx:crop(68,86,134,103)',
+				border: 'rgb(220,172,52),2',
+			},
+		});
+
+		p.ui.class('base-screen-top-button-2').extend('base-screen-top-button').set({
+			border: 'rgb(176,20,20),2',
+			background: 'interface.pcx:crop(68,2,134,19)',
+			_hover: {
+				background: 'interface.pcx:crop(68,23,134,40)',
+				border: 'rgb(136,12,12),2',
+			},
+			_active: {
+				background: 'interface.pcx:crop(68,23,134,40)',
+				border: 'rgb(228,120,24),2',
+			},
+		});
+
+		p.ui.class('base-screen-top-button-dropdown').extend('base-screen-top-button-2').set({
+			width: 20,
+			background: 'interface.pcx:crop(248,2,265,19)',
+			_hover: {
+				background: 'interface.pcx:crop(248,23,265,40)',
+			},
+			_active: {
+				background: 'interface.pcx:crop(248,44,265,61)',
+			},
+		});
+
+		this.buttons.button({
+			class: 'base-screen-top-button-1',
+			align: 'left',
+			left: 3,
+			text: 'EXPLORE',
+		});
+		this.buttons.button({
+			class: 'base-screen-top-button-1',
+			align: 'left',
+			left: 116,
+			text: 'DISCOVER',
+		});
+
+		this.buttons.button({
+			class: 'base-screen-top-button-dropdown',
+			align: 'left',
+			left: 230,
+		});
+		this.buttons.button({
+			class: 'base-screen-top-button-2',
+			align: 'center',
+			width: 170,
+			text: 'GOVERNOR',
+		});
+		this.buttons.button({
+			class: 'base-screen-top-button-dropdown',
+			align: 'right',
+			right: 230,
+		});
+
+		this.buttons.button({
+			class: 'base-screen-top-button-1',
+			align: 'right',
+			right: 116,
+			text: 'BUILD',
+		});
+		this.buttons.button({
+			class: 'base-screen-top-button-1',
+			align: 'right',
+			right: 3,
+			text: 'CONQUER',
+		});
+
 	},
 
 };
